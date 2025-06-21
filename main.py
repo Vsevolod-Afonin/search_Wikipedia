c3a30c7770627419fac5b24e55e72980d1da68ac
Vsevolod-Afonin <Vsevladey@yandex.ru>
2025-06-21 14:03:47 +0700

    main.py

--- main.py
+++ main.py
@@ -20,12 +20,49 @@ from selenium.webdriver.common.by import By
 import random
 import time
 
-
-
 wiki_url = ''
 browser = webdriver.Chrome()
 browser.get('https://ru.wikipedia.org/wiki/%D0%97%D0%B0%D0%B3%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0')
+def close():
+    quit()
 
+def par():
+    paragraphs = browser.find_elements(By.TAG_NAME, 'p')
+    for paragraph in paragraphs:
+        print(paragraph.text)
+        input()
+        menu()
+
+def go_page():
+    hatnotes = []
+
+    for element in browser.find_elements(By.TAG_NAME, 'div'):
+        cl = element.get_attribute('class')
+        if cl == 'hatnote navigation-not-searchable ts-main':
+            hatnotes.append(element)
+
+    print(hatnotes)
+    hatnote = random.choice(hatnotes)
+    link = hatnote.find_element(By.TAG_NAME, 'a').get_attribute('href')
+    browser.get(link)
+    time.sleep(1)
+
+    menu()
+
+
+def menu():
+    menu = input('Листать параграфы (Введите П)\n'  
+          'Перейти на одну из внутренних статей (Введите С)\n'
+          'Хотите закрыть программу?(Введите Д)')
+
+    if menu == 'Д':  # Закрыть
+        close()
+
+    if menu == 'П':  # Листать
+        par()
+
+    if menu == 'С':  # Перейти
+        go_page()
 
 try:
     while True:
@@ -38,68 +75,7 @@ try:
         search_box.send_keys(ask)
         time.sleep(1)
         search_box.send_keys(Keys.RETURN)
-        p_go_close = input('Листать параграфы (Введите П)\n'#
-                           'Перейти на одну из внутренних статей (Введите С)\n'
-                           'Хотите закрыть программу?(Введите Д)')
-        if p_go_close == 'Д': #Закрыть
-            break
+        menu()
 
-
-        if p_go_close == 'П': #Листать
-            paragraphs = browser.find_elements(By.TAG_NAME, 'p')
-            for paragraph in paragraphs:
-                print(paragraph.text)
-                input()
-                p_go_close = input('Листать параграфы (Введите Enter)\n'
-                                   'Перейти на одну из внутренних статей (Введите С)\n'
-                                   'Хотите закрыть программу?(Введите Д)')
-                if p_go_close == 'Д': ##
-                    break
-
-                if p_go_close == 'С': ##
-                    hatnotes = []
-
-                    for element in browser.find_elements(By.TAG_NAME, 'div'):
-                        cl = element.get_attribute('class')
-                        if cl == 'hatnote navigation-not-searchable ts-main':
-                            hatnotes.append(element)
-
-                    print(hatnotes)
-                    hatnote = random.choice(hatnotes)
-                    link = hatnote.find_element(By.TAG_NAME, 'a').get_attribute('href')
-                    browser.get(link)
-                    time.sleep(3)
-
-                    p_go_close = input('Листать параграфы (Введите П)\n'###
-                                       'Перейти на одну из внутренних статей (Введите Enter)\n'
-                                       'Хотите закрыть программу?(Введите Д)')
-
-                    if p_go_close == 'Д':  ###
-                        break
-
-                    if p_go_close == 'П':  ###
-                        paragraphs = browser.find_elements(By.TAG_NAME, 'p')
-                        for paragraph in paragraphs:
-                            print(paragraph.text)
-                            input()
-                            p_go_close = input('Листать параграфы (Введите Enter)\n'
-                                               'Перейти на одну из внутренних статей (Введите С)\n'
-                                               'Хотите закрыть программу?(Введите Д)')
-
-
-
-        if p_go_close == 'С': #Перейти
-            hatnotes = []
-
-            for element in browser.find_elements(By.TAG_NAME, 'div'):
-                cl = element.get_attribute('class')
-                if cl == 'hatnote navigation-not-searchable ts-main':
-                    hatnotes.append(element)
-
-            print(hatnotes)
-            hatnote = random.choice(hatnotes)
-            link = hatnote.find_element(By.TAG_NAME, 'a').get_attribute('href')
-            browser.get(link)
-            time.sleep(3)
 except:
-    print('Произошла ошибка')
+    print('Произошла ошибка!')
